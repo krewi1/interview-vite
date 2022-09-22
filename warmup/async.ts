3037ba5abd49ceaa1e4e1c9ca644081b5a7e8758
krewi <pavel@represent.com>
2022-09-22 08:58:05 +0200

    consistency

--- warmup/async.ts
+++ warmup/async.ts
@@ -7,17 +7,13 @@ const fetchGames = async () => {
   try {
     const res = await fetch("https://swapi.dev/api/planets/1");
     const data = await res.json();
-    return data;
-  } catch (e) {
-    return { success: false, error: e };
+    return { success: true, data };
+  } catch (error) {
+    return { success: false, error };
   }
 };
 
 (async () => {
-  try {
-    const result = await fetchGames();
-    console.log(result);
-  } catch (e) {
-    console.log(e);
-  }
+  const result = await fetchGames();
+  console.log(result);
 })();
--- warmup/promise.ts
+++ warmup/promise.ts
@@ -4,16 +4,14 @@ import fetch from "node-fetch";
 // How would you type the response
 
 const fetchPlanet = () => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fetch("https://swapi.dev/api/planets/1")
       .then((data) => data.json())
-      .then((data) => resolve({ data, success: true }))
-      .catch((e) => reject({ success: false, error: e }));
+      .then((data) => resolve({ success: true, data }))
+      .catch((error) => resolve({ success: false, error }));
   });
 };
 
 (() => {
-  fetchPlanet()
-    .then((response) => console.log(response))
-    .catch((error) => console.log("error", error));
+  fetchPlanet().then((response) => console.log(response));
 })();
